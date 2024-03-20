d1f550ec506a25b33c33bbb04976d76f701ccce0
Madelina11 <141474422+Madelina11@users.noreply.github.com>
2024-03-20 14:46:32 +0800

    Delete routes/web directory

--- routes/web/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthUser\UserController;
-
-Route::middleware(['auth', 'setLocale'])->prefix('/admin')->name('admin.')->group(function(){
-    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
-});
